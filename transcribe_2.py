00f7aa0aacf94978a0b85d5ffa477c32ada51a9e
geraldvillaran <gerald.villaran.dev@gmail.com>
2024-09-04 19:11:36 +0800

    correct typo

--- transcribe_2.py
+++ transcribe_2.py
@@ -1,28 +1,20 @@
-# TODO#1 - Import Libraries and Check availabity of GPU
+# TODO#1 - Import Libraries and Check availability of GPU
+
 
 def transcribe(file_path, model=None, language=None, verbose=False):    
     try:
         # TODO#2 - Import Libraries and Check availabity of GPU
 
-        
 
         # TODO#3 - Establish Access to Audio File
-
-        
         
         # TODO#4 - Perform transcription and store to `result`
-        
-        
 
         # TODO#5 - Create folder where the transcription will be saved to
 
-        
-
         # TODO#6 - Loop through the result segments.
-        pass #Delete this after TODO#2
-
         
-
+        pass #Delete this after TODO#2
     except RuntimeError:
         print("Not a valid file, skipping.")
 
